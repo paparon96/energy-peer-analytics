02848c57ee2c3e7e8e124ac0659f9ca5194b3e0f
paparon96 <>
2022-10-20 23:44:19 +0200

    Add donut chart of energy usage dist by devices

--- app.py
+++ app.py
@@ -66,3 +66,14 @@ f_imp = alt.Chart(feature_imp_df).mark_bar().encode(
 ).properties(height=700)
 st.subheader(f'Importance of different factors for energy usage')
 st.altair_chart(f_imp, use_container_width=True)
+
+# Energy usage by devices
+energy_usage_by_devices = pd.DataFrame({"device": ['Oven', 'Washing machine', 'Jacuzzi'],
+                                        "value": [4, 6, 10]})
+
+energy_by_dev = alt.Chart(energy_usage_by_devices).mark_arc(innerRadius=50).encode(
+    theta=alt.Theta(field="value", type="quantitative"),
+    color=alt.Color(field="device", type="nominal"),
+)
+st.subheader(f'Energy usage distribution among household devices')
+st.altair_chart(energy_by_dev, use_container_width=True)
